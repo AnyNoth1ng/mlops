3fce2f79a53d49ccf53cf072e8201bf472c67752
AnyNoth1ng <112391074+AnyNoth1ng@users.noreply.github.com>
2024-10-25 00:53:53 +0300

    Add files via upload

--- DAG.py
+++ DAG.py
@@ -3,13 +3,16 @@ from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
 from typing import Any, Dict, Literal
-
+import os
 import io
 import json
 import time
 import logging
 import pickle
+import mlflow
 import pandas as pd
+import datetime
+import numpy as np
 
 import warnings 
 warnings.filterwarnings('ignore')
@@ -30,6 +33,7 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.models import DAG, Variable
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
+from mlflow.models import infer_signature
 
 
 _LOG = logging.getLogger()
@@ -47,6 +51,15 @@ DEFAULT_ARGS = {
 }
 model_names = ["random_forest", "linear_regression", "desicion_tree"]
 
+def configure_mlflow():
+    for key in [
+        "MLFLOW_TRACKING_URI",
+        "AWS_ENDPOINT_URL",
+        "AWS_ACCESS_KEY_ID",
+        "AWS_SECRET_ACCESS_KEY",
+        "AWS_DEFAULT_REGION",
+        ]:
+            os.environ[key] = Variable.get(key) 
 
 models = dict(
     zip(model_names, [
@@ -59,169 +72,212 @@ X_cols = ['HouseAge','AveRooms','AveBedrms','Population','AveOccup','Latitude','
 y_cols = ['MedInc']
 
 
-def create_dag(dag_id: str, m_name: Literal["random_forest", "linear_regression", "desicion_tree"]):
-    dag = DAG(dag_id=dag_id,
-              schedule_interval = "0 1 * * * ",
-              start_date = days_ago(2),
-              catchup  = False
-                      
-             )
+dag = DAG(dag_id=f'{BASE_PATH}',
+            schedule_interval = "0 1 * * * ",   
+            start_date = days_ago(2),
+            catchup  = False,
+            tags = ["mlops"],
+            default_args = DEFAULT_ARGS
+            )
 
     ####### DAG STEPS #######
 
-    def init(m_name: Literal["random_forest", "linear_regression", "desicion_tree"]) -> Dict[str, Any]:
-        metrics = {}
-        metrics['model_name'] = m_name
-        metrics['start_time'] = datetime.now().strftime("%Y%m%d %H:%M%S")
-        
-        _LOG.info(f"начало пайплайна для {metrics['model_name']}")
-        
-        return metrics
+def init() -> Dict[str, Any]:
+    configure_mlflow()
+    metrics = {}
+    metrics['start_time'] = datetime.now().strftime("%Y%m%d %H:%M%S")
+    _LOG.info(f"Начало: {metrics['start_time']}")
+    
+    # exp_id = mlflow.get_experiment_by_name(BASE_PATH)
+    
+    try:
+        exp_id = mlflow.get_experiment_by_name(BASE_PATH)
+
+        if exp_id is not None:
+            exp_id = exp_id.experiment_id
+            _LOG.info(f'Эксперимент уже создан')
+        else:
+            exp_id = mlflow.create_experiment(BASE_PATH, artifact_location=f"s3://{BUCKET}/{BASE_PATH}")
+            _LOG.info(f"Создан эксперимент: {exp_id}")
+
+        mlflow.set_experiment(BASE_PATH)
+
+    except Exception as e:
+        _LOG.error(f"Ошибка с экспериментом :{e}")
+    
+    with mlflow.start_run(run_name='@Iam_PrP', experiment_id=exp_id, description='parent_run_id') as parent_run:
+        run_id = parent_run.info.run_id
+
+    metrics.update({
+        "run_id": run_id,
+        "experiment_name": BASE_PATH,
+        "experiment_id": exp_id
+    })
+    
+    return metrics
         
 
-    def get_data(**kwargs) -> Dict[str, Any]:
-        task_instance = kwargs['task_instance']
-        metrics = task_instance.xcom_pull(task_ids='init')
+def get_data(**kwargs) -> Dict[str, Any]:
+    task_instance = kwargs['task_instance']
+    metrics = task_instance.xcom_pull(task_ids='init')
+    metrics['start_time'] = datetime.now().strftime("%Y%m%d %H:%M%S")
+    
+    _LOG.info(f"Время и дата запуска дага: {metrics['start_time']}")
+    time_start = time.time()
+    # делаем датасет
+    calif = fetch_california_housing(as_frame=True)
+    df = pd.concat([calif["data"], pd.DataFrame(calif["target"])], axis=1)
+    # делаем коннект к хранилищу
+    s3_hook = S3Hook("s3_connection")
+    buffer = io.BytesIO()
+    df.to_pickle(buffer)
+    buffer.seek(0)
 
-        metrics['start_time'] = datetime.now().strftime("%Y%m%d %H:%M%S")
-        model_name = kwargs["model_name"]
-        
-        _LOG.info(f"Время и дата запуска дага и имя модели:{metrics['start_time']} {metrics['model_name']}")
-
-        time_start = time.time()
-        # делаем датасет
-        calif = fetch_california_housing(as_frame=True)
-        df = pd.concat([calif["data"], pd.DataFrame(calif["target"])], axis=1)
-        # делаем коннект к хранилищу
-        s3_hook = S3Hook("s3_connection")
-        buffer = io.BytesIO()
-        df.to_pickle(buffer)
-        buffer.seek(0)
 
     # сохраняем
+    s3_hook.load_file_obj(
+        file_obj=buffer,
+        key=f"{BASE_PATH}/datasets/california_housing.pkl",
+        bucket_name=BUCKET,
+        replace=True,
+    )
+    
+    time_end = time.time()
+    metrics['time_load'] = time_end - time_start
+    _LOG.info(f'время загрузки: {metrics["time_load"]}')
+
+    return metrics
+        
+
+def prepare_data(**kwargs) -> Dict[str, Any]:
+    ti = kwargs["ti"]
+    metrics = ti.xcom_pull(task_ids="get_data")
+    metrics["prerpoc_start"] = datetime.now().strftime("%Y%m%d %H:%M%S")
+    _LOG.info(f"Начало preprocessa: {metrics['prerpoc_start']}")
+    s3_hook = S3Hook("s3_connection")
+    file = s3_hook.download_file(key=f"{BASE_PATH}/datasets/california_housing.pkl", bucket_name=BUCKET)
+    df = pd.read_pickle(file)
+    
+    # preprocess
+    X = df[X_cols]
+    y = df[y_cols]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+    scal = StandardScaler()
+    X_train_scal = scal.fit_transform(X_train)
+    X_test_scal = scal.fit_transform(X_test)
+    X_train_scal = pd.DataFrame(X_train_scal, columns=X_cols, index=X_train.index)
+    X_test_scal = pd.DataFrame(X_test_scal, columns=X_cols, index=X_test.index)
+    for name, data in zip(["X_train", "X_test", "y_train", "y_test"], [X_train_scal,X_test_scal, y_train, y_test]):
+        buffer = io.BytesIO()
+        pickle.dump(data, buffer)
+        buffer.seek(0)
         s3_hook.load_file_obj(
             file_obj=buffer,
-            key=f"{BASE_PATH}/{model_name}/datasets/california_housing.pkl",
+            key=f"{BASE_PATH}/datasets/{name}.pkl",
             bucket_name=BUCKET,
             replace=True,
         )
-        
-        time_end = time.time()
-        metrics['time_load'] = time_end - time_start
-        _LOG.info(f'время загрузки: {metrics["time_load"]}')
+    metrics["prerpoc_end"] = datetime.now().strftime("%Y%m%d %H:%M%S")
+    _LOG.info(f"Конец preprocessa: {metrics['prerpoc_end']}")
+    return metrics
 
-        return metrics
-        
 
-    def prepare_data(**kwargs) -> Dict[str, Any]:
-        ti = kwargs["ti"]
-        metrics = ti.xcom_pull(task_ids="get_data")
-        model_name = kwargs["model_name"]
-        metrics["prerpoc_start"] = datetime.now().strftime("%Y%m%d %H:%M%S")
-        _LOG.info(f"Начало preprocessa: {metrics['prerpoc_start']}")
-        s3_hook = S3Hook("s3_connection")
-        file = s3_hook.download_file(key=f"{BASE_PATH}/{model_name}/datasets/california_housing.pkl", bucket_name=BUCKET)
-        df = pd.read_pickle(file)
-        
-        # preprocess
-        X = df[X_cols]
-        y = df[y_cols]
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
-        scal = StandardScaler()
+def train_model(**kwargs) -> Dict[str, Any]:
+    configure_mlflow()
+    ti = kwargs["ti"]
+    metrics = ti.xcom_pull(task_ids="prepare_data")
+    exp_id = kwargs['ti'].xcom_pull(key='experiment_id')
+    parent_run_id = kwargs['ti'].xcom_pull(key='parent_run_id')
 
-        X_train_scal = scal.fit_transform(X_train)
-        X_test_scal = scal.fit_transform(X_test)
+    st_train = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
-        X_train_scal = pd.DataFrame(X_train_scal, columns=X_cols)
-        X_test_scal = pd.DataFrame(X_test_scal, columns=X_cols)
+    model_name = kwargs["model_name"]
 
-        for name, data in zip(["X_train", "X_test", "y_train", "y_test"], [X_train_scal,X_test_scal, y_train, y_test]):
-            buffer = io.BytesIO()
-            pickle.dump(data, buffer)
-            buffer.seek(0)
-            s3_hook.load_file_obj(
-                file_obj=buffer,
-                key=f"{BASE_PATH}/{model_name}/datasets/{name}.pkl",
-                bucket_name=BUCKET,
-                replace=True,
-            )
+    s3_hook = S3Hook("s3_connection")
 
-        metrics["prerpoc_end"] = datetime.now().strftime("%Y%m%d %H:%M%S")
-        _LOG.info(f"Конец preprocessa: {metrics['prerpoc_end']}")
-
-        return metrics
-        
-
-    def train_model(**kwargs) -> Dict[str, Any]:
-        ti = kwargs["ti"]
-        metrics = ti.xcom_pull(task_ids="prepare_data")
-        model_name = kwargs["model_name"]
-
-        metrics["train_st"] = datetime.now().strftime("%Y%m%d %H:%M%S")
-        _LOG.info(f"Начало обучения: {metrics['train_st']}")
-
-        s3_hook = S3Hook("s3_connection")
-
-        data = {}
-        for name in ["X_train", "X_test", "y_train", "y_test"]:
-            file = s3_hook.download_file(
-                key=f"{BASE_PATH}/{model_name}/datasets/{name}.pkl",
-                bucket_name=BUCKET,
-            )
-            data[name] = pd.read_pickle(file)
-
-        model = models[model_name]
-        model.fit(data["X_train"], data["y_train"])
-        predict = model.predict(data["X_test"])
-    
-        result = {}
-        result["R2"] = r2_score(data["y_test"], predict)
-        result["rmse"] = mean_squared_error(data["y_test"], predict) ** 0.5
-        result["mae"] = median_absolute_error(data["y_test"], predict)
-
-        metrics["model_metrics"] = result
-        metrics["train_end"] = datetime.now().strftime("%Y%m%d %H:%M%S")
-        _LOG.info(f"Конец обучения: {metrics['train_end']}")
-        _LOG.info(f"Метрики: {metrics['model_metrics']}")
-
-        return metrics
-        
-        
-        
-
-    def save_results(**kwargs) -> None:
-        ti = kwargs["ti"]
-        metrics = ti.xcom_pull(task_ids="train_model")
-
-        _LOG.info(f"Метрики сохраненине результатов: {metrics}")
-
-        model_name = metrics["model_name"]
-
-        s3_hook = S3Hook("s3_connection")
-        buffer = io.BytesIO()
-        buffer.write(json.dumps(metrics).encode())
-        buffer.seek(0)
-        s3_hook.load_file_obj(
-            file_obj=buffer,
-            key=f"{BASE_PATH}/{model_name}/results/metrics.json",
+    data = {}
+    for name in ["X_train", "X_test", "y_train", "y_test"]:
+        file = s3_hook.download_file(
+            key=f"{BASE_PATH}/datasets/{name}.pkl",
             bucket_name=BUCKET,
-            replace=True
-            )
-        
-        _LOG.info("Всё сохранилось")
+        )
+        data[name] = pd.read_pickle(file)
+
     
+    X_val, X_test, y_val, y_test = train_test_split(data['X_test'], data['y_test'], test_size=0.5)
+ 
+    _LOG.info("Начинаем обучать модели")
 
-    with dag:
-        
-        task_init = PythonOperator(task_id = "init", python_callable = init, dag=dag, op_kwargs={"m_name": m_name})
-        task_get_data = PythonOperator(task_id = "get_data", python_callable = get_data, dag = dag, provide_context = True, op_kwargs= {"model_name": model_name})
-        task_prepare_data = PythonOperator(task_id = "prepare_data", python_callable = prepare_data, dag = dag, provide_context = True, op_kwargs= {"model_name": model_name})
-        task_train_model = PythonOperator(task_id = "train_model", python_callable = train_model, dag = dag, provide_context = True, op_kwargs={"model_name": model_name})
-        task_save_results = PythonOperator(task_id = "save_results", python_callable = save_results, dag = dag)
+    model = models[model_name]
     
+    with mlflow.start_run(parent_run_id=parent_run_id, run_name=model_name, experiment_id=exp_id, nested=True) as child_run:
+        model.fit(data["X_train"], data["y_train"])
+        fin_train = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
-    task_init >> task_get_data >> task_prepare_data >> task_train_model >> task_save_results
+        predict = model.predict(X_val)
+
+        val_data = X_val.copy()
+        val_data['target']= y_val
+        # _LOG.info(f'{model_name} -обучение ')
+        # _LOG.info(f'Форма X_train: {data["X_train"].shape}')
+        # _LOG.info(f'Форма y_train: {data["y_train"].shape}')
+        # _LOG.info(f'Форма X_test: {data["X_test"].shape}')
+        # _LOG.info(f'Форма y_test: {data["y_test"].shape}')
+
+        signature = infer_signature(X_test, predict)
+        model_info = mlflow.sklearn.log_model(model, model_name, signature=signature, registered_model_name=f"{model_name}")
+        mlflow.evaluate(
+            model=model_info.model_uri,
+            data=val_data,
+            targets="target",
+            model_type="regressor",
+            evaluators=["default"],
+        )
+        _LOG.info(f'{model} -обучилась ')
+    
+    kwargs["ti"].xcom_push(key=f'start-{model_name}', value=st_train)
+    kwargs["ti"].xcom_push(key=f'finish-{model_name}', value=fin_train)
 
 
-for model_name in models.keys():
-    create_dag(f"Polyakov_Egor_{model_name}", model_name)
+    _LOG.info("Конец обучения")
+        
+    return metrics
+
+def save_results(**kwargs) -> None:
+    
+    metrics = {}
+
+    metrics['train_linear_regression'] = kwargs["task_instance"].xcom_pull(task_ids="train_linear_regression")
+    metrics['train_random_forest'] = kwargs["task_instance"].xcom_pull(task_ids="train_random_forest")
+    metrics['train_desicion_tree'] = kwargs["task_instance"].xcom_pull(task_ids="train_desicion_tree")
+    
+    s3_hook = S3Hook("s3_connection")
+    
+    buffer = io.BytesIO()
+    buffer.write(json.dumps(metrics).encode())
+    buffer.seek(0)
+    s3_hook.load_file_obj(
+        file_obj=buffer,
+        key=f"{BASE_PATH}/results/metrics.json",
+        bucket_name=BUCKET,
+        replace=True
+        )
+    
+    _LOG.info("Всё сохранилось")
+
+with dag:
+    
+    task_init = PythonOperator(task_id = "init", python_callable = init, dag=dag)
+    task_get_data = PythonOperator(task_id = "get_data", python_callable = get_data, dag = dag)
+    task_prepare_data = PythonOperator(task_id = "prepare_data", python_callable = prepare_data, dag = dag)
+    task_train_model = [
+        PythonOperator(task_id=f"{model_name}", python_callable=train_model, dag=dag, provide_context=True, op_kwargs={"model_name": model_name}) for model_name in models.keys()
+        ]
+    task_save_results = PythonOperator(task_id = "save_results", python_callable = save_results, dag = dag)
+
+configure_mlflow()
+
+
+task_init >> task_get_data >> task_prepare_data >> task_train_model >> task_save_results
+
+
